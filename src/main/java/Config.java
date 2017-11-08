32411836d607e6dd11687c202d76f67892b59594
Perek Michal <perekmichal93@gmail.com>
2017-11-08 17:01:08 +0100

    whatever

---
+++ src/main/java/Config.java
@@ -0,0 +1,4 @@
+public class Config {
+    public static final String APP_ID = "AIzaSyAF4qJa02KubhGk7Ih61nWp5fJ3nGQsKnw";
+    public static final String APP_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/output?parameters";
+}
--- src/main/java/Controller.java
+++ src/main/java/Controller.java
@@ -1,2 +1,4 @@
+package pl.perekwilan;
+
 public class Controller {
 }
