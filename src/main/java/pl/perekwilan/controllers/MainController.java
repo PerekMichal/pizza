d72fece212fcd607270ec958ee62eaff9b251f2a
Artur Wilanowski <arturwilanowski@gmail.com>
2017-11-08 17:23:23 +0100

    edit.1

---
+++ src/main/java/pl/perekwilan/controllers/MainController.java
@@ -0,0 +1,23 @@
+package pl.perekwilan.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+
+public class MainController {
+
+    @FXML
+    Button ButtonMakePizz;
+
+    @FXML
+    TextField GiveCity;
+
+    @FXML
+    Label TheBestPizza;
+
+
+
+
+}
