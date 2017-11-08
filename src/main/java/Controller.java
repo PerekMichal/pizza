bd9739346c6be5f40287c298787326bac983145c
Artur Wilanowski <arturwilanowski@gmail.com>
2017-11-08 17:21:27 +0100

    edit.1

--- src/main/java/Controller.java
+++
@@ -1,4 +0,0 @@
-package pl.perekwilan;
-
-public class Controller {
-}
--- src/main/java/pl/perekwilan/utils/Config.java
+++ src/main/java/pl/perekwilan/utils/Config.java
@@ -1,4 +1,8 @@
+package pl.perekwilan.utils;
+
 public class Config {
     public static final String APP_ID = "AIzaSyAF4qJa02KubhGk7Ih61nWp5fJ3nGQsKnw";
     public static final String APP_URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/output?parameters";
+
+
 }
